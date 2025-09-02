2028775dea75630eb1131fefc0e5b137ad9a24f3
Taranto1987 <tarantoadspro@gmail.com>
2025-09-02 16:40:59 -0300

    Atualiza site Castor com versão otimizada e motor de recomendação

--- src/actions.ts
+++
@@ -1,5 +0,0 @@
-export const sendContact = (formData: FormData) => {
-    // Aqui você pode adicionar lógica para enviar os dados do formulário
-    console.log('Form data received:', formData);
-    return 'sucesso';
-};
