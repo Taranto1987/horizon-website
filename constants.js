ea1cd5daafb897ef8b47f992af5c8b92ac4d46fd
Taranto1987 <tarantoadspro@gmail.com>
2025-09-02 17:16:46 -0300

    Adiciona versão final otimizada do site Castor

---
+++ constants.js
@@ -0,0 +1,21 @@
+// Constants for Castor algorithm
+export const MEDIDAS_PADRAO_MM = {
+  SOLTEIRO_88x188: [880, 1880],
+  CASAL_138x188:   [1380, 1880],
+  QUEEN_158x198:   [1580, 1980],
+  KING_193x203:    [1930, 2030],
+};
+export const MEDIDAS_OPCIONAIS_MM = {
+  SOLTEIRAO_96x203: [960, 2030],
+  VIUVA_128x188:    [1280, 1880],
+};
+export const CLASSES_TECNICAS = [
+  'ESPUMA_D28','ESPUMA_D33','ESPUMA_D45',
+  'MOLAS_POCKET','MOLAS_MINIPOCKET',
+  'HIBRIDO_POCKET_ESPUMA',
+  'ONE_FACE','DOUBLE_FACE',
+  'PILLOW_TOP','EURO_PILLOW',
+];
+export const FIRMEZA_ESCALA = [1,2,3,4,5];
+export const ORIGEM_PRECO_VALIDA = ['input_manual'];
+export const RESPONSAVEIS_PRECO = ['Thalles','Marcela'];
